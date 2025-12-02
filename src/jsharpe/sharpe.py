ed4282570d49c00e888f0ed79119b3fa2f07d05c
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-12-02 10:55:35 +0400

    test minimum variance

--- src/jsharpe/sharpe.py
+++ src/jsharpe/sharpe.py
@@ -22,6 +22,42 @@ def ppoints(n, a=None):
     return np.linspace(1 - a, n - a, n) / (n + 1 - 2 * a)
 
 
+def robust_covariance_inverse( V: np.ndarray ) -> np.ndarray:
+    r"""
+    Inverse of a constant-correlation covariance matrix, using the Sherman–Morrison formula
+
+    Assume $V = \rho \sigma \sigma' + (1-\rho) \text{diag}(\sigma^2)$ (variance matrix, with constant correlations).
+    Its inverse is $V^{-1} = A^{-1} - \dfrac{ A^{01} \rho \sigma \sigma' A^{-1} }{ 1 + \rho \sigma' A^{-1} \sigma }$.
+
+    Input:
+    - V: np.ndarray, variance matrix
+    Output:
+    - np.ndarray, inverse of the variance matrix
+    """
+    sigma = np.sqrt( np.diag(V) )
+    C = (V.T/sigma).T/sigma
+    rho = np.mean( C[ np.triu_indices_from(C,1) ] )
+    A = np.diag( 1 / sigma**2 ) / (1-rho)
+    sigma = sigma.reshape( -1, 1 )
+    return A - ( rho * A @ sigma @ sigma.T @ A ) / ( 1 + rho * sigma.T @ A @ sigma )
+
+
+def minimum_variance_weights_for_correlated_assets(V: np.ndarray) -> np.ndarray:
+    """
+    Weights of the minimum variance portfolio, for correlated assets, assuming a constant-correlation covariance matrix
+
+    Input:
+    - V: np.ndarray, variance matrix, shape (n,n)
+    Output:
+    - np.ndarray, weights of the minimum variance portfolio, shape (n,)
+    """
+    ones = np.ones( shape = V.shape[0] )
+    S = robust_covariance_inverse(V)
+    w = S @ ones
+    w = w / np.sum(w)
+    return w
+
+
 if __name__ == "__main__":
     x = ppoints(20)
     print(x)
---
+++ tests/test_minimum_variance.py
@@ -0,0 +1,22 @@
+import cvxpy as cp
+import numpy as np
+
+from jsharpe.sharpe import minimum_variance_weights_for_correlated_assets
+
+
+def test_minimum_variance_weights_for_correlated_assets():
+    np.random.seed(0)
+    rho = .5
+    C = rho * np.ones( shape=(10,10) )
+    np.fill_diagonal(C, 1)
+    sigma = np.random.lognormal( size=10 ).reshape( -1, 1 )
+    V = (C*sigma).T*sigma
+    w = minimum_variance_weights_for_correlated_assets(V)
+
+    W = cp.Variable( shape = V.shape[0] )
+    problem = cp.Problem(
+        cp.Minimize( cp.quad_form(W, V) ),
+        [W.sum() == 1]
+    )
+    problem.solve()
+    assert np.all( np.abs( W.value - w ) < 1e-10 )
